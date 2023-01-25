36130025ad39f9c0cbe67c5dcc9d93ffc74889c8
Cameron Clark <crclark95@gmail.com>
2023-01-25 15:45:50 -0700

    Remove inline script

---
+++ chart.js
@@ -0,0 +1,6 @@
+// want more comparisons? add more colors to this spot and then make a new css
+// class with that color (see the inline css in the svg and simulation.css)
+var chart_colors_class = ["blue", "red", "green"];
+var margin = {top: 20, right: 20, bottom: 30, left: 100},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
---
+++ svg.js
@@ -0,0 +1,1 @@
+d3.select("#saveSVG").on("click", saveAsSvg);
